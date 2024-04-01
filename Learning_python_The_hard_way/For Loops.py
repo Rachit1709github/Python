a49a103ff6f36331c2fdd7b8e940ae4dbae029f1
Git1709 <rachitsharma1709@gmail.com>
2024-04-02 02:18:38 +0530

    Restarting python

---
+++ Learning_python_The_hard_way/For Loops.py
@@ -0,0 +1,36 @@
+x = "Abishek Bacchan"
+for i in x:
+    print(i, end=",")
+print("\n")
+for i in x:
+    print(i)
+print("\n")
+print("\n")
+for i in x:
+
+    if (i == "b" or i == "B"):
+        print("this is special ")
+    else:
+        print(i)
+
+print("\n")
+print("\n")
+print("\n")
+list1 = ["red", "blue", "green", "alpha", "orange", "purple"]
+colors = ["red", "blue", "green", "alpha", "orange", "purple", [1, 2, 3, 4, 5, 6, 7, 8]]
+
+for i in list1:
+    print(i)
+print("\n")
+print("\n")
+print("\n")
+for j in colors:
+    print(j)
+for k in range(20):
+    print(k)
+for k in range(20):
+    print(k + 1)
+for k in range(19990, 20000):
+    print(k)
+for k in range(0, 20, 5):
+    print(k )
